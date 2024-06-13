2b235dc4af1a7158d8d20bf18ac92cffff42008b
LuciaOliveraa <lucia.olivera@correo.ucu.edu.uy>
2024-06-12 22:13:25 -0300

    Round Robin funcionando en consola.

--- demo/src/main/java/com/example/Fettuccine.java
+++ demo/src/main/java/com/example/Fettuccine.java
@@ -3,20 +3,54 @@ package com.example;
 public class Fettuccine extends Thread implements IPiatto {
     long tempoDiCottura = 6000;
     final long timeOut = LaCosaNostra.getInstance().timeOut;
+    long alDente = 4000;
+    boolean alDenteDone = false;
 
     public long getTempoDiCottura() {
         return tempoDiCottura;
     }
 
     public void run() {
-        long initialTime = System.currentTimeMillis();
-        long currentTime = 0;
+        //long initialTime = System.currentTimeMillis();
+        //long currentTime = initialTime;
         long elapsedTime = 0;
 
+        // Se encarga de e/s
+        while ((tempoDiCottura > 0) && (elapsedTime < timeOut) && (alDente > 0)) {
+            elapsedTime += 1000;
+            tempoDiCottura -= 1000;
+            alDente -= 1000;
+        }
+
+        // Se encarga exclusivamente del tiempo total y timeOut
+        while ((tempoDiCottura > 0) && (elapsedTime < timeOut) && alDenteDone) {
+            elapsedTime += 1000;
+            tempoDiCottura -= 1000;
+            alDente = 0;
+        }
+
+        if (alDente == 0) alDenteDone = true;
+
+        try {
+            Thread.sleep(elapsedTime); // Simula el tiempo de procesamiento
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Se encarga de e/s
+        /*while (elapsedTime < timeOut && elapsedTime < alDente) {
+            currentTime = System.currentTimeMillis();
+            elapsedTime = currentTime - initialTime;
+            tempoDiCottura = tempoDiCottura - elapsedTime;
+            alDente = alDente - elapsedTime;
+        }
+        alDente = 0;
+
+        // Se encarga exclusivamente del tiempo total y timeOut
         while (elapsedTime < timeOut && elapsedTime < tempoDiCottura) {
             currentTime = System.currentTimeMillis();
             elapsedTime = currentTime - initialTime;
             tempoDiCottura = tempoDiCottura - elapsedTime;
-        }
+        }*/
     }
 }
--- demo/src/main/java/com/example/Tiramisu.java
+++ demo/src/main/java/com/example/Tiramisu.java
@@ -9,14 +9,25 @@ public class Tiramisu extends Thread implements IPiatto {
     }
 
     public void run() {
-        long initialTime = System.currentTimeMillis();
-        long currentTime = 0;
+        /*long initialTime = System.currentTimeMillis();
+        long currentTime = initialTime;
         long elapsedTime = 0;
 
         while (elapsedTime < timeOut && elapsedTime < tempoDiCottura) {
             currentTime = System.currentTimeMillis();
             elapsedTime = currentTime - initialTime;
             tempoDiCottura = tempoDiCottura - elapsedTime;
+        }*/
+        long elapsedTime = 0;
+        while ((tempoDiCottura > 0) && (elapsedTime < timeOut)) {
+            elapsedTime += 1000;
+            tempoDiCottura -= 1000;
+        }
+
+        try {
+            Thread.sleep(elapsedTime); // Simula el tiempo de procesamiento
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
